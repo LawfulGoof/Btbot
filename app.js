6b4a625750668fd552b00b39751ae11d1d54e5c3
LawfulGoof <38697994+LawfulGoof@users.noreply.github.com>
2018-07-26 15:18:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,70 +1,142 @@
-// Load up the discord.js library
-const Discord = require("discord.js");
+import discord
+import asyncio
+import re
+import datetime
+import math
 
-// This is your client. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
-const client = new Discord.Client();
+# A pattern to match the word vore, and only the single word vore.
+pattern = re.compile(r'\b[V|v][O|Ò|Ó|Ô|Õ|Ö|o|ò|ó|ô|õ|ö][R|r][E|È|É|Ê|Ë|e|è|é|ê|ë][S|s]?\b')
+serverAndDate = {}
+botStartup = datetime.datetime.now()
+lastMention = {}
+awake = {}
 
-// Here we load the config.json file that contains our token and our prefix values. 
-const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
+client = discord.Client()
 
-client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  client.user.setActivity(`Beneath the Basement on YT`);
-});
+def readTimesFromFile():
+    global serverAndDate
+    with open("timeStamps.txt", "r") as target:
+        for line in target:
+            tmp = line.split(',')
+            tmp[1] = tmp[1][0:-1]
+            serverAndDate[tmp[0]] = datetime.datetime.strptime(tmp[1], "%Y-%m-%d %H:%M:%S")
+            if (len(tmp) >= 3):
+                awake[tmp[0]] = bool(tmp[2])
+            else:
+                awake[tmp[0]] = True
+            
 
-client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Beneath the Basement on YT`);
-});
-
-client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Beneath the Basement on YT`);
-});
+def writeTimesToFile():
+    with open('timeStamps.txt', 'w') as target:
+        for serverId in serverAndDate:
+            target.write('{},{},{}\n'.format(serverId, serverAndDate[serverId].strftime("%Y-%m-%d %H:%M:%S"), awake[serverId]))
 
 
-client.on("message", async message => {
-  // This event will run on every single message received, from any channel or DM.
-  
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
-  if(message.author.bot) return;
-  
-  // Also good practice to ignore any message that does not start with our prefix, 
-  // which is set in the configuration file.
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  // Here we separate our "command" name, and our "arguments" for the command. 
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-  // Let's go with a few common example commands! Feel free to delete or change those.
-  
-  if(command === "help") {
-    const m = await message.channel.send("I am useless");
-  }
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
-  if(command === "repeat") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
+@client.event
+async def on_message(message):
+    global botStartup
+    global serverAndDate
+    global lastMention
+    global awake
+    currentTime = datetime.datetime.now()
+    serverId = message.server.id
+    
+    ''' I'm just using startup time instead of join date, because that makes way more sense for older
+        servers if the filesystem stuff doesn't work
+    '''
+    #bot = None
+    #for x in message.server.members:
+    #    if client.user.id == x.id:
+    #        bot = x
+    #        break
+
+    # Timezone hack, apparently isn't needed for Heroku.
+    lastReferenced = botStartup
+    if serverId in serverAndDate:
+        lastReferenced = serverAndDate[serverId]
+    if serverId not in lastMention:
+        bot = None
+        for x in message.server.members:
+            if client.user.id == x.id:
+                bot = x
+                break
+        lastMention[serverId] = bot.joined_at
+    if serverId not in awake:
+        awake[serverId] = True
+
+    # Begin time formatting
+    diff = currentTime - lastReferenced
+    hours = math.floor(diff.seconds/3600)
+    minutes = math.floor((diff.seconds - hours * 3600)/60)
+    seconds = diff.seconds - hours * 3600 - minutes * 60
+    dt = "{} days, ".format(diff.days)
+    ht = "{} hours, ".format(hours)
+    mt = "{} minutes, and ".format(minutes)
+    st = "{} seconds".format(seconds)
+
+    if diff.days == 1:
+        dt = "1 day, "
+    elif diff.days == 0:
+        dt = ""
+        if hours == 0:
+            ht = ""
+            mt = "{} minutes and ".format(minutes)
+            if minutes == 0:
+                mt = ""
+
+    if hours == 1:
+        ht = "1 hour, "
+    if minutes == 1:
+        if ht == "":
+            mt = "1 minute and "
+        else:
+            mt = "1 minute, and "
+    if seconds == 1:
+        st = "1 second"
+    # End Time formatting stuff
+
+    if (hasattr(message.author, 'server_permissions')):
+        permission = message.author.server_permissions
+        if message.content.startswith('!vtsilence') and permission.administrator:
+            await client.send_message(message.channel, "Ok {}, I'll be quiet now. use '!vtalert' to wake me back up!".format(message.author.mention))
+            awake[serverId] = False
+            writeTimesToFile()
+        elif message.content.startswith('!vtalert') and permission.administrator:
+            await client.send_message(message.channel, "Ok {}, I'm scanning now.".format(message.author.mention))
+            awake[serverId] = True
+            writeTimesToFile()
+    if message.content.startswith('!vtsilence') or message.content.startswith('!vtalert'):
+        pass
+    elif message.content.startswith('!vthelp'):
+        await client.send_message(message.channel, "You can ask me how long we've made it with '!vt'.\n If you're an admin you can silence me with '!vtsilence' and wake me back up with '!vtalert'")
+    elif message.content.startswith('!vt'):
+        await client.send_message(message.channel, 'The server has gone {}{}{}{} without mentioning the forbidden word.'.format(dt, ht, mt, st))
+    if ((pattern.search(message.content) is not None) and (message.author.id != client.user.id)):
+        serverAndDate[serverId] = currentTime
+        writeTimesToFile()
+        print ("{}::: {} lasted {} seconds.".format(currentTime, serverId, (currentTime - lastMention[serverId]).total_seconds()))
+        if (awake[serverId] and (currentTime - lastMention[serverId]).total_seconds() >= 1800):
+            await client.send_message(message.channel, '{} referenced the forbidden word, setting the counter back to 0. I\'ll wait a half hour before warning you again.\n The server went {}{}{}{} without mentioning it.'.format(message.author.mention, dt, ht, mt, st))
+            lastMention[serverId] = currentTime
+
+
+readTimesFromFile()
+print('Stored server info:')
+count = 0
+for id in serverAndDate:
+    print ("{}: id: {}, time: {}".format(count, id, serverAndDate[id]))
+    count = count + 1
+
+with open("key.txt", "r") as target:
+        for line in target:
+            client.run(line)
   
   
 });
